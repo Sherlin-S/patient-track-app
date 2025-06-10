c660e83e051e3314581a38e8bf216f5255cfe3e8
Sherlin-S <sherlinkmr@gmail.com>
2025-06-10 22:33:05 +0530

    Update and rename next.config.js to next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "www.logopeople.in",
+      "interhospi.com",
+      "www.shutterstock.com",
+      "www.ripplesiot.com",
+      "www.ripplesiot.com"
+    ],
+  },
+};
+
+module.exports = nextConfig;
